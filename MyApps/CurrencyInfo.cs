a7d5d2cac59a8b7869af3ced40e05b7b982efd4b
Ivan Martinovic <makz66@hotmail.com>
2023-12-23 16:14:58 +0100

    Started working on currency converter GUI. Added class to represent currency information and basic logic to retrieve currency list from HNB (Croatian National Bank) API.

---
+++ MyApps/CurrencyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApps
+{
+    internal class CurrencyInfo
+    {
+        private string _Currency { get; set; }
+        private float _BuyingRate { get; set; }
+        private float _SellingRate { get; set; }
+
+        public CurrencyInfo(string currency, float buyingRate, float sellingRate) {
+            _Currency = currency;
+            _BuyingRate = buyingRate;
+            _SellingRate = sellingRate;
+        }
+    }
+}
--- MyApps/MainWindow.xaml.cs
+++ MyApps/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Net.NetworkInformation;
 
 namespace MyApps
 {
@@ -23,6 +27,32 @@ namespace MyApps
         public MainWindow()
         {
             InitializeComponent();
-        }
+
+            //hnb tečajna lista
+            try
+            {
+                var url = "https://api.hnb.hr/tecajn-eur/v3";
+                JArray parsedResponse;
+                var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpRequest.Method = "GET";
+                var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+
+                List<CurrencyInfo> currencyList = new List<CurrencyInfo>();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var responseJson = streamReader.ReadToEnd();
+                    parsedResponse = JArray.Parse(responseJson);
+                }
+                foreach (var currency in parsedResponse)
+                {
+                    currencyList.Add(new CurrencyInfo((string)currency["valuta"], (float)currency["kupovni_tecaj"], (float)currency["prodajni_tecaj"]));
+                }
+            }
+            catch (WebException)
+            {
+
+                MessageBox.Show("Failed to load currency rate. Using outdated rates. Check your internet connection. ");
+            }          
+        }       
     }
 }
